90324c1d3a25eba8ec4f97fee966cf418f56fce4
Jeferson Costa <rc.jeferson@gmail.com>
2022-12-17 14:13:08 -0300

    feat(mail): implements mail example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MailService } from './mail/mail.service';
+import { PostmarkMailService } from './mail/postmark-mail.service';
+import { SMTPMailService } from './mail/smtp-mail.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: MailService,
+      useClass: SMTPMailService,
+    }
+  ],
 })
 export class AppModule {}
